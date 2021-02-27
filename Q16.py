655335a017f16d4bf7cf953ce5347b0e62e2f7eb
alperkarasuer <>
2021-02-27 21:05:43 +0300

    Add files via upload

--- Q16.py
+++
@@ -1,5 +0,0 @@
-myNum = 2**1000
-
-myNumStr = str(myNum)
-digits = [int(x) for x in myNumStr]
-print(sum(digits))
--- Q20.py
+++
@@ -1,7 +0,0 @@
-import math
-
-myNum = math.factorial(100)
-
-myNumStr = str(myNum)
-digits = [int(x) for x in myNumStr]
-print(sum(digits))
